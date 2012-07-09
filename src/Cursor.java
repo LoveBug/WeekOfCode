8981a2a13c803dc00fcb8df64545472d9cc9c2d0
Scott Weston <westonscot@kahlos.ecs.vuw.ac.nz>
2012-07-10 09:23:26 +1200

    inital wok on Cursor class and getting mouse input ready

---
+++ src/Cursor.java
@@ -0,0 +1,41 @@
+import java.awt.Graphics;
+
+
+public class Cursor {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	
+	private ImageWrapper cursor;
+	
+	public Cursor(int x, int y, int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		this.setX(x);
+		this.setY(y);
+		cursor = new ImageWrapper("images/cursor.gif");  //needs to be changed when initial cursor is made
+	}
+	
+	public void draw(Graphics g)
+	{
+		cursor.draw(g, x, y, width, height);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
