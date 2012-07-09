e6495ca690386ab9dff7018d8b32b21343fb29df
Scott Weston <westonscot@alley-cat.ecs.vuw.ac.nz>
2012-07-09 13:21:20 +1200

    Changes to initial commit

--- src/ImageWrapper.java
+++ src/ImageWrapper.java
@@ -1,4 +1,58 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import javax.imageio.ImageIO;
 
 public class ImageWrapper {
+	private BufferedImage picture;
+	private Color transparency;
+
+	public ImageWrapper(String location, Color colorKey) {
+		File myfile = new File(location);
+
+		try {
+			picture = ImageIO.read(myfile);
+
+		} catch (IOException e) {
+			System.out.println("the error is " + e);
+		}
+
+		transparency = colorKey;
+	}
+
+	public ImageWrapper(String location) {
+		File myfile = new File(location);
+
+		try {
+			picture = (BufferedImage)ImageIO.read(myfile);
+
+		} catch (IOException e) {
+			System.out.println("the error is " + e);
+		}
+		
+		transparency = new Color(picture.getRGB(0, 0));
+	}
+	
+	public void draw(Graphics g, int x, int y, int width, int height)
+	{
+		g.drawImage(picture, x, y, width, height, null);
+	}
+
+	public Image getImage() {
+		return (picture);
+	}
+
+	public void setImage(BufferedImage tempImage) {
+		picture = tempImage;
+	}
+
+	public Color getTranspercy() {
+		return transparency;
+	}
+
+	public void setTranspercy(Color transparency) {
+		this.transparency = transparency;
+	}
 
 }
