3df6f2de58f2d30d67c3a4e1ed7e07c4b62899dc
Michael Blockley <blocklmich@alley-cat.ecs.vuw.ac.nz>
2012-07-10 14:33:21 +1200

    Map Conflict

--- src/Map.java
+++ src/Map.java
@@ -33,7 +33,6 @@ public class Map {
 	public Map(String filename) {
 	// TODO Auto-generated constructor stub
 		worldSprites = new SpriteSheet("images/platformTiles.gif");
-		charSprites = new SpriteSheet("images/runCyclePrelimSheet.gif");
 		inputfile =  new File(filename);
 		try {
 			 scan = new Scanner(inputfile);
@@ -57,7 +56,7 @@ public class Map {
 				for(int i = 0; i< xDimension; i++){
 					int item = scan.nextInt();
 					if(item==16){
-						character = new Character(i*32,j*32, charSprites);
+						character = new Character(i*32,j*32);
 						map[i][j] = new BackgroundTile(32,32,i*32,j*32,TILE_DEPTH,new ImageWrapper(-1, 32, 32,worldSprites));
 					}else{
 						ImageWrapper imgwrap = new ImageWrapper(item, 32, 32, worldSprites);
