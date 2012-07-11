0965de47797d24eb4c11555c3f48c5e02055dc94
Scott Weston <westonscot@alley-cat.ecs.vuw.ac.nz>
2012-07-12 09:56:47 +1200

    enemy is now moving properly

--- src/sprites/Enemy.java
+++ src/sprites/Enemy.java
@@ -1,27 +1,38 @@
 package sprites;
+import java.awt.Graphics;
+
 import swarm.Hitbox;
 import map.Map;
 
 
 public class Enemy extends Sprite{
 	
+	private boolean lastDirection;
+	
 	public Enemy(int x, int y, int width, int height, String runCycle,
 			int frames) {
 		super(x, y, width, height, runCycle, frames);
+		getImage().reverse();
 		setDirection(false);
 	}
 	
 	public void move(Map m){
 		int x = getX()-1;
 		if(getDirection())
-			x+=getWidth()-1;
-		Hitbox temp = new Hitbox(getMovementBox().getX()-5, getMovementBox().getY()-5,
-				getMovementBox().getWidth()+5, getMovementBox().getHeight()+5);
-		for(int y=getY()-1; y<getY()+getHeight()+1; y++){
-			if(temp.checkCollision(m.getMap()[x/32][y/32].getHitbox()))
+			x+=getWidth()+2;
+		Hitbox temp = new Hitbox(x, getMovementBox().getY()-5,
+				x+1, getMovementBox().getHeight()+5);
+		
+		for(int y=getY()+1; y<getY()+getHeight()-1; y++){
+			if(temp.checkCollision(m.getMap()[x/32][y/32].getHitbox())){
+				
 				setDirection(!getDirection());
+				break;
+			}
 		}
+		
 		super.walk(getDirection());
 		super.move(m);
 	}
+	
 }
