39a00680fad39887076e599c1408fedaaa7e3d33
whyteferg <lexusbeat@gmail.com>
2012-07-12 10:05:29 +1200

    Adding hud updates and Items

---
+++ src/map/Hud.java
@@ -0,0 +1,20 @@
+package map;
+
+import java.util.ArrayList;
+
+import sprites.Item;
+import sprites.Character;
+
+public class Hud {
+	Character character;
+	ArrayList<Item> characterItems;
+	
+	 Hud(Character character) {
+		 
+		 this.character = character;
+		 this.characterItems = character.getItemList();		
+	}
+	
+	
+	
+}
