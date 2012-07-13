c5c0f68dd66abed05faef81e7e7207953f4e8248
Scott Weston <westonscot@kahlos.ecs.vuw.ac.nz>
2012-07-13 14:25:08 +1200

    initial work on heart

---
+++ src/items/Heart.java
@@ -0,0 +1,12 @@
+package items;
+
+public class Heart extends Item {
+
+	public Heart(int x, int y, int width, int height, String Spritesheet,
+			int spritenumber) {
+		super(x, y, width, height, Spritesheet, spritenumber);
+		setMoveDistance(0);
+	}
+	
+	
+}
